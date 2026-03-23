2f39fb392c37813514ebdd66e00e4b1e5dd903d9
samkv2 <shivamkv92@gmail.com>
2026-03-24 00:00:30 +0530

    Add PowerSet.java to unit7

---
+++ unit7/PowerSet.java
@@ -0,0 +1,39 @@
+package unit7;
+
+import java.util.ArrayList;
+
+public class PowerSet {
+
+    public static ArrayList<ArrayList<Integer>> getSubsets(ArrayList<Integer> set, int index) {
+        ArrayList<ArrayList<Integer>> allsubsets;
+        if (set.size() == index) { // Base case - add empty set
+            allsubsets = new ArrayList<ArrayList<Integer>>();
+            allsubsets.add(new ArrayList<Integer>());
+        } else {
+            allsubsets = getSubsets(set, index + 1);
+            int item = set.get(index);
+            ArrayList<ArrayList<Integer>> moresubsets = new ArrayList<ArrayList<Integer>>();
+            for (ArrayList<Integer> subset : allsubsets) {
+                ArrayList<Integer> newsubset = new ArrayList<Integer>();
+                newsubset.addAll(subset);
+                newsubset.add(item);
+                moresubsets.add(newsubset);
+            }
+            allsubsets.addAll(moresubsets);
+        }
+        return allsubsets;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> set = new ArrayList<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+        ArrayList<ArrayList<Integer>> subsets = getSubsets(set, 0);
+        System.out.println("Power Set of " + set + ":");
+        for (ArrayList<Integer> subset : subsets) {
+            System.out.println(subset);
+        }
+    }
+}
